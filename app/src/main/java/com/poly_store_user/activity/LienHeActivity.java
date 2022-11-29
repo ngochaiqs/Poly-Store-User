17dbdd1ef10d6ec962206e2374b0eddac0e845fb
NgocHai <luongngochaiqs@gmail.com>
2022-11-29 14:24:24 +0700

    Sửa giao diện đơn hàng

--- app/src/main/java/com/poly_store_user/activity/LienHeActivity.java
+++ app/src/main/java/com/poly_store_user/activity/LienHeActivity.java
@@ -1,16 +1,29 @@
 package com.poly_store_user.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.poly_store_user.R;
 
 public class LienHeActivity extends AppCompatActivity {
-
+Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lien_he);
+
+        toolbar = findViewById(R.id.toolbarLienHe);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Liên Hệ");
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
