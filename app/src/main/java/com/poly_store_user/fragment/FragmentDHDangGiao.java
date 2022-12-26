b9e56e620baff55ccac2f5f9ba42f5b7a2cc5a16
legon <legonvhien9.3@gmail.com>
2022-12-26 23:55:01 +0700

    tab don hang

--- app/src/main/java/com/poly_store_user/fragment/FragmentDHDangGiao.java
+++ app/src/main/java/com/poly_store_user/fragment/FragmentDHDangGiao.java
@@ -8,13 +8,47 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.poly_store_user.R;
+import com.poly_store_user.adapter.DonHangAdapter;
+import com.poly_store_user.retrofit.ApiBanHang;
+import com.poly_store_user.retrofit.RetrofitClient;
+import com.poly_store_user.utils.Utils;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class FragmentDHDangGiao extends Fragment {
+    CompositeDisposable compositeDisposable = new CompositeDisposable();
+    ApiBanHang apiBanHang;
+    RecyclerView redonhang;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragmentdhdanggiao_layout,container, false);
+        View view = inflater.inflate(R.layout.fragmentdhdanggiao_layout, container, false);
+
+        apiBanHang = RetrofitClient.getInstance(Utils.BASE_URL).create(ApiBanHang.class);
+        redonhang = view.findViewById(R.id.recycleview_donhangdanggiao);
+        redonhang.setLayoutManager(new LinearLayoutManager(getContext()));
+        donHangDangGiao();
+        return view;
+    }
+
+    private void donHangDangGiao() {
+        compositeDisposable.add(apiBanHang.xemDonHang(Utils.nguoidung_current.getMaND(),2)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        donHangModel -> {
+                            DonHangAdapter adapter = new DonHangAdapter(getContext(), donHangModel.getResult());
+                            redonhang.setAdapter(adapter);
+                        },
+                        throwable -> {
+
+                        }
+                ));
     }
 }
